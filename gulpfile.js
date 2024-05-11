442507de85ff678134ddacdc7632f13db842bd10
Rafael Antonny V C <rafaelantonny6@gmail.com>
2024-05-11 13:34:52 -0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+
+function compileSass() {
+    return gulp.src('./src/styles/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = compileSass;
+exports.watch = function() {
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(compileSass))
+}
